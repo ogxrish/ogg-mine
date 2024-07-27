6cf502be5c8d6e6b7f441144e53c5589de74299a
josh handelman <handelmanjos@gmail.com>
2024-07-26 17:21:16 -0700

    stuff

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -3,7 +3,14 @@ import { changeData, fund, getGlobalAccount, getProgramBalance, getProgramSolBal
 import { useWallet } from "@solana/wallet-adapter-react";
 import { useEffect, useState } from "react";
 
-const verifiedWallets = ["FUcoeKT9Nod5mWxDJJrbq4SycLAqNyxe5eMnmChbZ89p", "58V6myLoy5EVJA3U2wPdRDMUXpkwg8Vfw5b6fHqi2mEj"]; // use this to kick non admin users off the page. 
+const verifiedWallets = [
+    "FUcoeKT9Nod5mWxDJJrbq4SycLAqNyxe5eMnmChbZ89p",
+    "58V6myLoy5EVJA3U2wPdRDMUXpkwg8Vfw5b6fHqi2mEj",
+    "9cpGSYpRthttGo3QvidzWbd3nseHP3fGSURQvqsih7dw",
+    "96tzHrW9WyUeNWfRSL6WHnRMUGcs57f6vvZqcYnr8Fxy",
+    "3nX6bZAgGT3ys2NEQNegLdLuUkeppVXhbZPLzMcVzsqw",
+    "GztqFTzUhVRtQUPcgC6rsL36BpkZJ7edTc5XsStHj9fF",
+]; // use this to kick non admin users off the page. 
 // this is not strictly necessary as only admin can call the withdraw functions in the contract
 export default function Admin() {
     const { publicKey } = useWallet();
